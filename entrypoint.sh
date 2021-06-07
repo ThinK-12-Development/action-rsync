5679c5e3c061a1d622e77dca6421f117f11a425a
Tyler O'Brien <hello@tylerobrien.com>
2021-06-06 22:52:46 -0400

    Fix wrong oper overwriting ssh_config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ echo " StrictHostKeyChecking=no" >> /etc/ssh/ssh_config
 echo " IdentityFile ~/.ssh/remote.key" >> /etc/ssh/ssh_config
 echo " ProxyJump proxy" >> /etc/ssh/ssh_config
 
-echo "Host proxy" > /etc/ssh/ssh_config
+echo "Host proxy" >> /etc/ssh/ssh_config
 echo " User $INPUT_PROXY_USER" >> /etc/ssh/ssh_config
 echo " HostName $INPUT_PROXY_HOST" >> /etc/ssh/ssh_config
 echo " StrictHostKeyChecking=no" >> /etc/ssh/ssh_config
