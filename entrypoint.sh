ee8a847e824c567dc93b4f6e27a341c8378b5c42
Tyler O'Brien <hello@tylerobrien.com>
2021-06-06 21:53:50 -0400

    Fix rsync timing out

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,39 +2,60 @@
 
 set -eu
 
-EXCLUSIONS_FILE=exclusions
+#
+# Prepare SSH config
+#
 
-REMOTE_SSH_KEY_FILE=remote.key
-REMOTE_DESTINATION=$INPUT_USER@$INPUT_HOST:$INPUT_DESTINATION
-REMOTE_SHELL="ssh -o StrictHostKeyChecking=no -i $REMOTE_SSH_KEY_FILE"
+mkdir .ssh
 
-PROXY_SSH_KEY_FILE=proxy.key
-PROXY_COMMAND="ssh -o StrictHostKeyChecking=no -i $PROXY_SSH_KEY_FILE $INPUT_PROXY_USER@$INPUT_PROXY_HOST -W %h:%p"
-PROXY_SHELL="ssh -o 'ProxyCommand $PROXY_COMMAND'"
+echo "$INPUT_KEY" > remote.key
+echo "$INPUT_PROXY_KEY" > proxy.key
 
-echo ".cache" > "$EXCLUSIONS_FILE"
-echo ".docker" > "$EXCLUSIONS_FILE"
-echo ".env" > "$EXCLUSIONS_FILE"
-echo ".*.env" > "$EXCLUSIONS_FILE"
-echo ".git" > "$EXCLUSIONS_FILE"
-echo ".github" > "$EXCLUSIONS_FILE"
-echo ".storybook" > "$EXCLUSIONS_FILE"
-echo ".vscode" > "$EXCLUSIONS_FILE"
-echo "docker-compose.yml" > "$EXCLUSIONS_FILE"
-echo "Dockerfile" > "$EXCLUSIONS_FILE"
+echo "Host destination" > .ssh/config
+echo " User $INPUT_USER" >> .ssh/config
+echo " HostName $INPUT_HOST" >> .ssh/config
+echo " StrictHostKeyChecking=no" >> .ssh/config
+echo " IdentityFile ~/.ssh/remote.key" >> .ssh/config
+echo " ProxyJump proxy" >> .ssh/config
 
-echo "$INPUT_KEY" > $REMOTE_SSH_KEY_FILE
-echo "$INPUT_PROXY_KEY" > $PROXY_SSH_KEY_FILE
+echo "Host proxy" > .ssh/config
+echo " User $INPUT_PROXY_USER" >> .ssh/config
+echo " HostName $INPUT_PROXY_HOST" >> .ssh/config
+echo " StrictHostKeyChecking=no" >> .ssh/config
+echo " IdentityFile ~/.ssh/proxy.key" >> .ssh/config
 
-chmod 400 $EXCLUSIONS_FILE
-chmod 400 $REMOTE_SSH_KEY_FILE
-chmod 400 $PROXY_SSH_KEY_FILE
+chmod 600 .ssh
+chmod 400 .ssh/config
+chmod 400 remote.key
+chmod 400 proxy.key
+
+mv remote.key .ssh
+mv proxy.key .ssh
+
+#
+# Add default file exclusions
+#
+
+echo ".cache" > exclusions
+echo ".docker" >> exclusions
+echo ".env" >> exclusions
+echo ".*.env" >> exclusions
+echo ".git" >> exclusions
+echo ".github" >> exclusions
+echo ".storybook" >> exclusions
+echo ".vscode" >> exclusions
+echo "docker-compose.yml" >> exclusions
+echo "Dockerfile" >> exclusions
+
+chmod 400 exclusions
+
+#
+# Main
+#
 
 rsync --archive \
       --compress \
       --quiet \
       --recursive \
-      --exclude-from "$EXCLUSIONS_FILE" \
-      --rsh "$PROXY_SHELL" \
-      --rsh "$REMOTE_SHELL" \
-      "$INPUT_SOURCE" "$REMOTE_DESTINATION"
+      --exclude-from exclusions \
+      "$INPUT_SOURCE" destination:"$INPUT_DESTINATION"
